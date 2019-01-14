2105f0e9be494a994e4c4c83910783263c127632
awkonecki <awkonecki@google.com>
2019-01-13 16:14:19 -0800

    [FTR] Stack Library Build Support
    Initial support for building and running local instance for the library
    definition for the stack portion of the container library.
    
    Related Issues
    - #30

---
+++ data-structures/stacks/src/main/java/com/stack/Stack.java
@@ -0,0 +1,8 @@
+import arrays.src.main.java.com.array.Array;
+
+public class Stack<T> {
+    public static void main(String [] args) {
+        Array<Integer> myArray = new Array<Integer>(10);
+        System.out.println(myArray.capacity());
+    }
+}
